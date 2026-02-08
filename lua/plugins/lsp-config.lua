44ec9ed8231745d71b55d01ef2894bb8b10ee3b9
lalleydev <samuellalley@gmail.com>
2026-02-08 07:09:42 -0800

    Finished LSP install and added keybinds
    Note: .lurarc.json are global definitions so "vim" is does not throw an
    error in the ide

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,35 @@
 return{
     {
+        -- installs and manages lsps
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
         end
     },
     {
+        -- ensures that lsps are installed
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
+                -- add the language server here first
                 ensure_installed = {
-                    "lua_ls"
+                    "lua_ls",
                 }
             })
         end
     },
     {
+        -- config the lsp interactions
+        -- between neovim and lsp
         "neovim/nvim-lspconfig",
         config = function()
+            -- lsp servers
             vim.lsp.enable('lua_ls')
-            --vim.lsp.config('lua_ls',{
 
-            --})
-            --local lspconfig = vim.lsp.config()
-            --lspconfig.lua_ls.setup({})
+            -- lsp config keybinds
+            vim.keymap.set('n','K',vim.lsp.buf.hover,{})
+            vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
+            vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
         end
     },
 }
