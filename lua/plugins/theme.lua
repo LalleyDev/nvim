331e4123c8261851e3b4943f489722f01905fbbf
116rep <samuellalley@gmail.com>
2026-03-01 12:21:27 -0800

    Added tokyonight

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,31 @@
-local theme = "cat"
+local theme = "tokyo"
+-- local theme = "cat"
+-- local theme = "default"
 
 if theme == "tokyo" then
     return{
         "folke/tokyonight.nvim",
-        lazy = true,
+        lazy = false,
         opts = { style = "moon" },
+        config = function()
+            -- night, storm, day, moon
+            vim.cmd.colorscheme "tokyonight"
+        end
     }
+elseif theme == "cat" then
+    return{
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            -- latte, frappe, macchiato, mocha
+            vim.cmd.colorscheme "catppuccin-macchiato"
+        end
+    }
+elseif theme == "default" then
+    return{
+    }
+
 elseif theme == "cat" then
     return{
         "catppuccin/nvim",
